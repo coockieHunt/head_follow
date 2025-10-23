786bbcd769ad85a90ccfd2f6660bd3ccd3a5baba
coockie_hunt <jonathan.gleyze@gmail.com>
2025-10-23 08:32:10 +0200

    Add license and source for the sound played when copying a quote

--- src/utils/copyQuote.jsx
+++ src/utils/copyQuote.jsx
@@ -1,6 +1,10 @@
+import WriteSong from '../assets/write.ogg';
+
 function copyQuote(quote) {
   navigator.clipboard.writeText(quote).then(() => {
     console.log('Quote copied to clipboard');
+    const audio = new Audio(WriteSong);
+    audio.play();
   }).catch(err => {
     console.error('Error copying quote: ', err);
   });
